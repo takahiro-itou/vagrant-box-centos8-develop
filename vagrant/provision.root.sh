6cf24c007f89489e2b609708124c0c1bb37ea3a1
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-01-18 08:20:25 +0900

    インストールパッケージを追加
    (1) clang をインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,4 +6,5 @@ sudo  timedatectl  set-timezone Asia/Tokyo
 
 sudo  yum  update -y
 sudo  yum  groupinstall -y  "Base"  "Development Tools"
-sudo  yum  install -y  cmake  emacs  mercurial
+sudo  yum  install -y  cmake  emacs  git  mercurial  \
+    clang
